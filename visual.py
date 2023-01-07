0701d2ed17e56af41438e01fe41c94808aca9a84
Dmitriy <gidrevich.work@yandex.ru>
2023-01-07 22:49:32 +0400

    second commit

--- visual.py
+++ visual.py
@@ -1,1 +1,14 @@
-import PyQT
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+import sys
+
+def application():
+    app = QApplication(sys.argv)
+    main_window = QMainWindow
+
+    main_window.setWindowTitle("Рассылка трек-номеров")
+    main_window.setGeometry(300, 250, 350, 200)
+
+    main_window.show()
+    sys.exit(app.exec_())
